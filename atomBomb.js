c5db0e1d8002906b630f519d944a70cd3aba4d74
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-10-24 00:19:06 +0400

    Squashed 'LogicalOperators-js/' content from commit 40e45b1
    git-subtree-dir: LogicalOperators-js
    git-subtree-split: 40e45b16ad56b5754afe53cf82ce13091fe8c553

---
+++ atomBomb.js
@@ -0,0 +1,20 @@
+const presidentKey = false;
+const primeMinisterKey = false;
+const generalOfArmiesKey = false;
+const masterKey = false;
+
+
+// ატომური ბომბი გაეშვება 2 შემთხვევაში:
+
+// თუ პრეზიდენტი, პრემიერი და გენერალი, სამივე ერთად არიან თანახმები.
+// თუ ცუდმა ბიძიებმა მასტერ გასაღები გამოიყენეს.
+
+
+// გამოთვალეთ შედეგი და ჩაწერეთ boolean ცვლადში:
+
+// let bombWillLaunch = (presidentKey && primeMinisterKey && generalOfArmiesKey) || masterKey;
+
+// console.log(bombWillLaunch);
+
+
+console.log(0 && 30)
